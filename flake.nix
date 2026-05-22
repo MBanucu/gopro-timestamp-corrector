28b106f3655ce55b8af79713e6934fa4ed54ac7f
MBanucu <michael.banucu@googlemail.com>
2026-05-22 15:48:19 +0200

    chore: add uname output in VM test

--- flake.nix
+++ flake.nix
@@ -74,6 +74,8 @@
           bin_path = "${nixosPkgs.lib.makeBinPath nixosTestDeps}"
           python_bin = "${nixosTestPython}/bin/python3"
 
+          machine.succeed("echo '--- NixOS test: VM kernel ---'")
+          machine.succeed("uname -a")
           machine.succeed("echo '--- NixOS test: copying source ---'")
           machine.succeed("echo '--- NixOS test: environment check ---'")
           machine.succeed(
